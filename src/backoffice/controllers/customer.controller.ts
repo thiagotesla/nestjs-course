49f2248aa3ae6df701c7b276fee24758d8d4d9dc
thiagotesla <thiagobarros95@gmail.com>
2022-01-12 11:04:32 -0300

    feat: adiciona CRUD genérico ao CustomerController

--- src/backoffice/controllers/customer.controller.ts
+++ src/backoffice/controllers/customer.controller.ts
@@ -1,4 +1,20 @@
 import { Controller } from "@nestjs/common";
 
 @Controller()
-export class CustomerController{}
+export class CustomerController{
+    get() {
+        return 'Método Get';
+    }
+
+    post(){
+        return 'Método Post';
+    }
+
+    put() {
+        return 'Método Put';
+    }
+
+    delete(){
+        return 'Método Delete';
+    }
+}
