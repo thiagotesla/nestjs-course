612eb300750186c5d29a93f179a00657a61e9954
thiagotesla <thiagobarros95@gmail.com>
2022-01-12 13:53:24 -0300

    feat: define parametros e seus tipos na CustomerModel

--- src/backoffice/models/customer.model.ts
+++ src/backoffice/models/customer.model.ts
@@ -1,5 +1,10 @@
 export class Customer{
     constructor(
+        public name: string,
+        public document: string,
+        public email: string,
+        public password: string,
+        public active: boolean,
     ){
     }
 }
