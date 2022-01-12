31a08c2a58f7bc43ff409e34de596758d34bafed
thiagotesla <thiagobarros95@gmail.com>
2022-01-12 14:37:20 -0300

    feat: padroniza retorno das requisições no CustomerController

--- src/backoffice/controllers/customer.controller.ts
+++ src/backoffice/controllers/customer.controller.ts
@@ -1,32 +1,58 @@
 import { Body, Controller, Delete, Get, Param, Post, Put } from "@nestjs/common";
 import { Customer } from "../models/customer.model";
+import { Result } from "../models/result.model";
 
 @Controller('v1/customers')
 export class CustomerController{
     @Get()
     get() {
-        return 'Método Get';
+        return new Result(
+        'Lista de todos os clientes',
+        [],
+        true,
+        null
+        );
     }
     @Get(':document')
-    getById(@Param('document') document){
-        return 'Método GetById ' + document;
+    getById(@Param('document')document ){
+        return new Result(
+        'cliente de CPF ' + document +".",
+        {},
+        true,
+        null);
     }
 
     @Post()
     post(@Body() body: Customer){
-        return 'cliente ' + body.name + ' criado com sucesso! ' + 'email: ' + body.email + '!';
+        return new Result(
+        'Cliente criado com sucesso!',
+        [
+        body.name,
+        body.email,
+        body.document
+        ],
+        true,
+        null
+        );
     }
 
     @Put(':document')
     put(@Param('document') document, @Body() body) {
-        return {
-            customer: document,
-            data: body,
-        };
+        return new Result(
+        'Cliente atualizado com sucesso!',
+        body.name,
+        true,
+        null
+        );
     }
 
     @Delete(':document')
-    delete(@Param('document') document){
-        return 'Usuário ' + document + ' deletado!';
+    delete(@Param('document') document, @Body()body: Customer){
+        return new Result(
+        'Cliente ' + body.name + ' deletado com sucesso',
+        null,
+        true, 
+        null
+        );
     }
 }
