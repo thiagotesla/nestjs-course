37ca87dbc37296d48a1b48e38a4030e683938ce3
thiagotesla <thiagobarros95@gmail.com>
2022-01-12 10:59:56 -0300

    feat: adiciona CustomerController ao BackofficeModule

--- src/backoffice/backoffice.module.ts
+++ src/backoffice/backoffice.module.ts
@@ -1,4 +1,7 @@
 import { Module } from "@nestjs/common";
+import { CustomerController } from "./controllers/customer.controller";
 
-@Module({})
+@Module({
+    controllers: [CustomerController],
+})
 export class BackofficeModule{}
