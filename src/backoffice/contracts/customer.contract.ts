9fe28b70b8ffdff8c705dd230b4f4973020bc8ba
thiagotesla <thiagobarros95@gmail.com>
2022-01-24 09:54:51 -0300

    feat: adiciona validações do customer model ao customer contract

--- src/backoffice/contracts/customer.contract.ts
+++ src/backoffice/contracts/customer.contract.ts
@@ -1,3 +1,16 @@
-export class CreateCustomerContract{
-    
+import { Flunt } from "src/utils/flunt";
+import { Customer } from "../models/customer.model";
+import { Contract } from "./contract";
+
+export class CreateCustomerContract implements Contract{
+    errors: any[];
+
+    validate(model: Customer): any {
+        const flunt = new Flunt();
+
+        flunt.hasMinLen(model.name, 2, "Nome inválido.");
+        flunt.isEmail(model.email, "Email inválido.");
+        flunt.isFixedLen(model.document, 11, "O CPF deve ter 11 caracteres.");
+        flunt.hasMinLen(model.password, 6, "Senha inválida.")
+    }
 }
