517ed72a1d64ff7ab287bae7d423a9c21644c30a
thiagotesla <thiagobarros95@gmail.com>
2022-01-24 11:04:38 -0300

    refactor: adiciona user model como parametro do customer model

--- src/backoffice/models/customer.model.ts
+++ src/backoffice/models/customer.model.ts
@@ -1,18 +1,18 @@
 import { Adress } from "./adress.model";
 import { CreditCard } from "./credit-card.model";
 import { Pet } from "./pets.model";
+import { User } from "./user.model";
 
 export class Customer{
     constructor(
         public name: string,
         public document: string,
         public email: string,
-        public password: string,
-        public active: boolean,
         public pets: Pet[],
         public billingAdress: Adress[],
         public shippingAdress: Adress[],
         public creditcard: CreditCard[],
+        public user: User,
     ){
     }
 }
