0385da80189ee11bc9de9ee6966e75b8a9e1ab20
thiagotesla <thiagobarros95@gmail.com>
2022-01-25 08:51:40 -0300

    feat: adiciona AccountService aos providers do backofficemodule

--- src/backoffice/backoffice.module.ts
+++ src/backoffice/backoffice.module.ts
@@ -3,6 +3,7 @@ import { MongooseModule, Schema } from "@nestjs/mongoose";
 import { CustomerController } from "./controllers/customer.controller";
 import { CustomerSchema } from "./schemas/customer.schema";
 import { UserSchema } from "./schemas/user.schema";
+import { AccountService } from "./services/account.service";
 
 @Module({
     imports:[
@@ -18,5 +19,6 @@ import { UserSchema } from "./schemas/user.schema";
         ])
     ],
     controllers: [CustomerController],
+    providers:[AccountService],
 })
 export class BackofficeModule{}
