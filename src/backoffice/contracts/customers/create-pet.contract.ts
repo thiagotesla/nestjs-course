9d7fa4668d715c29ab9b2f3f949adbba5665d255
thiagotesla <thiagobarros95@gmail.com>
2022-01-25 15:22:17 -0300

    feat: adiciona create pet contract

---
+++ src/backoffice/contracts/customers/create-pet.contract.ts
@@ -0,0 +1,19 @@
+import { Pet } from "src/backoffice/models/pets.model";
+import { Flunt } from "src/utils/flunt";
+import { Contract } from "../contract";
+
+export class CreatePetContract implements Contract{
+    errors: any[];
+    validate(model: Pet): any {
+        
+        const flunt = new Flunt();
+        flunt.hasMinLen(model.name, 2, 'Nome inválido para um pet.');
+        flunt.hasMinLen(model.gender, 3, 'Gênero inválido.');
+        flunt.hasMinLen(model.kind, 3, 'Tipo inválido.'),
+        flunt.hasMinLen(model.breed, 3, 'Raça inválida.');
+
+        this.errors = flunt.errors;
+        
+        return flunt.isValid();
+    }
+}
