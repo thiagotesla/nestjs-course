5dea80c2c2de48c68cd71b795f6aef48e2c6da1e
thiagotesla <thiagobarros95@gmail.com>
2022-01-25 11:00:29 -0300

    feat: cria metodo AddBillingAddress

--- src/backoffice/services/customer.service.ts
+++ src/backoffice/services/customer.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from "@nestjs/common";
 import { InjectModel } from "@nestjs/mongoose";
 import { Model } from "mongoose";
+import { Address } from "../models/address.model";
 import { Customer } from "../models/customer.model";
 
 @Injectable()
@@ -11,6 +12,15 @@ export class CustomerService {
     async create(data: Customer): Promise<Customer>{
         const customer = new this.model(data);
         return await customer.save();
+    }
+
+    async AddBillingAddress(document: string, data: Address): Promise<Customer>{
+        const options = {upsert: true};
+        return await this.model.findOneAndUpdate({document}, { 
+            $set: {
+                billingAddress: data,
+            }
+        }, options) 
     }
 
 }
