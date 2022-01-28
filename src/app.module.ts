ab24c4ccf106c0c63d37beb1f7c9eb16e04eeb68
thiagotesla <thiagobarros95@gmail.com>
2022-01-28 09:49:18 -0300

    feat: configura module typeorm no app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,26 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { BackofficeModule } from './modules/backoffice/backoffice.module';
+import { StoreModule } from './modules/store/store.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import 'dotenv/config'
 
 @Module({
   imports: [
+  
     MongooseModule.forRoot(process.env.CONNECTION_STRING),
-    BackofficeModule
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PW,
+      database: process.env.DB_NAME,
+      entities: [__dirname + '/**/*/.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+    BackofficeModule,
+    StoreModule
   ],
   controllers: [],
   providers: [],
