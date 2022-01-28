2ed1bb2b61eda9615fc6ee4746b3af6c13820887
thiagotesla <thiagobarros95@gmail.com>
2022-01-28 11:56:09 -0300

    feat: configura imports, controllers e providers do store module

--- src/modules/store/store.module.ts
+++ src/modules/store/store.module.ts
@@ -1,4 +1,18 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ProductController } from './controllers/product.controller';
+import { Product } from './entities/product.entity';
+import { ProductService } from './services/product.service';
 
-@Module({})
+@Module({
+    imports: [TypeOrmModule.forFeature([
+        Product,
+    ])],
+    controllers: [
+        ProductController,
+    ],
+    providers:[
+        ProductService,
+    ],
+})
 export class StoreModule {}
