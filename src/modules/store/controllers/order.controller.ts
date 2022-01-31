eeff387e9485f151d547bde847b888914b826ddc
thiagotesla <thiagobarros95@gmail.com>
2022-01-31 14:38:02 -0300

    feat: cria order controller

---
+++ src/modules/store/controllers/order.controller.ts
@@ -0,0 +1,77 @@
+import { Body, Controller, Get, HttpException, HttpStatus, Param, Post } from "@nestjs/common";
+import { Result } from "src/modules/backoffice/models/result.model";
+import { OrderItemDto } from "../dtos/order-item.dto";
+import { OrderItem } from "../entities/order-item.entity";
+import { Order } from "../entities/order.entity";
+import { OrderItemService } from "../services/order-item.service";
+import { OrderService } from "../services/order.service";
+import { ProductService } from "../services/product.service";
+
+@Controller('v1/orders')
+export class OrderController{
+    constructor(
+        private readonly orderService: OrderService,
+        private readonly orderItemService: OrderItemService,
+        private readonly productService: ProductService,
+    ){}
+
+    @Get(':order')
+    async get(@Param('order') order: string){
+        try{
+            const orders = await this.orderService.getByNumber(order);
+            return new Result(null, orders, true, null)
+        }
+        catch(error){
+            throw new HttpException(new Result(
+            'Não foi possível listar os pedidos. Confira seus dados.',
+            false,
+            null,
+            error),
+            HttpStatus.BAD_REQUEST
+        );
+    }
+    }
+    @Get(':customer')
+    async getByCutomer(@Param('customer') customer: string){
+        try{
+            const orders = await this.orderService.getByCustomer(customer);
+            return new Result(null, orders, true, null)
+        }
+        catch(error){
+            throw new HttpException(new Result(
+            'Não foi possível listar os pedidos. Confira seus dados.',
+            false,
+            null,
+            error),
+            HttpStatus.BAD_REQUEST
+        );
+
+        }
+    }
+
+    @Post()
+    async post(@Body() model: OrderItemDto[]) {
+        try {
+            let order = new Order();
+            order.customer = '12345678911';
+            order.date = new Date();
+            order.number = '1B2D3F5';
+            order.items = [];
+            await this.orderService.post(order);
+
+            for (const item of model) {
+                let product = await this.productService.findById(item.product);
+                let orderItem = new OrderItem();
+                orderItem.order = order;
+                orderItem.product = product;
+                orderItem.price = product.price;
+                orderItem.quantity = item.quantity;
+                await this.orderItemService.post(orderItem);
+            };
+
+            return new Result(null, model, true,  null);
+        } catch (error) {
+            throw new HttpException(new Result('Não foi possível criar seu pedido', false, null, error), HttpStatus.BAD_REQUEST);
+        }
+    }
+}
