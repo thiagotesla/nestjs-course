aee84a8a4064d5793602e7c42a494ae34d3eefaf
thiagotesla <thiagobarros95@gmail.com>
2022-02-01 16:27:29 -0300

    feat: cria jwt-strategy

---
+++ src/shared/strategies/jwt-stretagy.ts
@@ -0,0 +1,28 @@
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { PassportStrategy } from '@nestjs/passport';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { AuthService } from '../services/auth.service';
+import { JwtPayload } from '../interfaces/jwt-payload.interface';
+
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(
+    private readonly authService: AuthService,
+  ) 
+  {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: process.env.SECRET_KEY,
+    });
+  }
+
+  async validate(payload: JwtPayload) {
+    const user = await this.authService.validateUser(payload);
+    if(!user){
+      throw new UnauthorizedException();
+    }
+    return user;
+  }
+}
