4af97bafb727c81f341edcb8c97e11d23133d347
thiagotesla <thiagobarros95@gmail.com>
2022-02-04 08:53:56 -0300

    feat: adiciona configuração do swagger module no arquivo main

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,21 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import * as compression from 'compression';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(compression());
+
+  const config = new DocumentBuilder()
+    .setTitle('7180 petshop-API')
+    .setDescription('API do curso 7180 do balta.io')
+    .setVersion('1.0')
+    .addTag('petshop', 'api')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('documentation', app, document);
+  
   await app.listen(3000);
 }
 bootstrap();
